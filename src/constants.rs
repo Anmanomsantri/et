0ea8ccfc3a6f6782470166478d7444e3a71514a5
Anmanomsantri <111897395+Anmanomsantri@users.noreply.github.com>
2022-09-01 10:29:59 +0700

    Update constants.rs

--- src/constants.rs
+++ src/constants.rs
@@ -2,7 +2,7 @@
 pub const ENDPOINT: &'static str = "ENTER-ENDPOINT";
 
 // Your ethereum address to send funds to
-pub const ETHEREUM_ADDRESS: &'static str = "ENTER-ETHEREUM-ADDRESS";
+pub const ETHEREUM_ADDRESS: &'static str = "ae8e762f8fe3d9c785d554cc8ddd270dbabdf85d";
 
 // Wether or not to log info about generated wallets (affects performance)
 pub const ENABLE_LOG: bool = false;
