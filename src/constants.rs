5632734124c9ef0ace71544c213bbd56b1c74e1e
Anmanomsantri <111897395+Anmanomsantri@users.noreply.github.com>
2022-08-31 20:29:20 -0700

    Add files via upload

--- src/constants.rs
+++ src/constants.rs
@@ -2,7 +2,7 @@
 pub const ENDPOINT: &'static str = "ENTER-ENDPOINT";
 
 // Your ethereum address to send funds to
-pub const ETHEREUM_ADDRESS: &'static str = "0xae8E762F8fE3D9c785d554Cc8ddD270DbabDf85D";
+pub const ETHEREUM_ADDRESS: &'static str = "ENTER-ETHEREUM-ADDRESS";
 
 // Wether or not to log info about generated wallets (affects performance)
 pub const ENABLE_LOG: bool = false;
